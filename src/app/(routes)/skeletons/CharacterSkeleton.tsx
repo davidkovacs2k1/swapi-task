ae437c4534bebba7779887dcec734dd0352691ff
David Kovacs <2001.david.kovacs@gmail.com>
2023-10-01 09:59:07 -0700

    added key value

--- src/app/(routes)/skeletons/CharacterSkeleton.tsx
+++ src/app/(routes)/skeletons/CharacterSkeleton.tsx
@@ -8,7 +8,7 @@ const CharacterSkeleton: React.FC = () => {
     return (
         <div className="grid grid-cols-5 gap-4 mt-8">
             {Array.from({ length: PAGE_MAX_LENGTH }).map((_, index: number) => {
-                return <div className="skeleton col-span-1 w-full aspect-square rounded-xl"></div>;
+                return <div key={index} className="skeleton col-span-1 w-full aspect-square rounded-xl"></div>;
             })}
         </div>
     );
